cdc18aced823381efddd6ea8460a606ff103edff
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-07-27 17:04:47 +0300

    added search and category item component

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Text, StyleSheet } from "react-native";
+import CategoryItem from "./src/components/CategoryItem";
 import Header from "./src/components/Header";
+import Search from "./src/components/Search";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Search />
+      <CategoryItem />
+      <CategoryItem />
+      <CategoryItem />
+      <CategoryItem />
       <StatusBar />
     </View>
   );
